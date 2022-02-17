5878fe7c39eb781e7b5037750d87209df9b3bcb3
sSs811 <78473376+sSs811@users.noreply.github.com>
2022-02-17 21:48:00 +0700

    Add files via upload

---
+++ lab1/lab1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+  class Programm
+  {
+    static void Main(string[] args)
+     {
+     HW1.QueueTime(new int[] { 5, 3, 4 }, 1);
+     HW1.QueueTime(new int[] { 10, 2, 3, 3 }, 2);
+     HW1.QueueTime(new int[] { 2, 3, 10 }, 2);
+     HW1.QueueTime(new int[] { 11, 2, 3, 5 }, 3);
+     HW1.QueueTime(new int[] { 7, 6, 10 }, 3);
+        }
+  }
+    
+    
+public class HW1
+   {
+    public static long QueueTime(int[] customers, int n)
+     {
+
+      int vv,i;
+      int ix = n - 1;
+      int ch=0;
+      int[] kass = new int[n];
+ 
+      for (i = 0; i < n; i++)
+       {
+        kass[i] = customers[i];
+       } 
+
+      while(true) 
+       {
+
+        for (i = 0; i < n; i++)
+         {
+
+          kass[i]= kass[i]- 1;
+          ch = ch+ 1;
+          if (kass[i] == 0)
+           {
+
+            if (2<=ix && kass.Max() == 0)
+             {
+
+              while(ch % n != 0)
+               {
+
+                ch= ch + 1;
+               }
+
+               vv=ch/n ;
+               Console.WriteLine("Время обслуживания: ");
+               Console.WriteLine(vv);
+              return 0;
+             }
+
+            else if (ix < 2)
+             {
+              kass[i] = customers[ix+1];
+              ix+= 1;
+             }
+           }
+         }
+       }
+     }
+  }
+}
